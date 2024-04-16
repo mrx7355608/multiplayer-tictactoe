86528b13c2e0c6b50266a102d9f9da37bc59f914
mrx7355608 <mrx7355608@gmail.com>
2024-04-17 04:45:00 +0500

    initialized socket server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+import { Server } from "socket.io";
 import app from "./app.js";
 import http from "http";
+import sessionMiddleware from "./sessionMiddleware.js";
 
 const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+io.engine.use(sessionMiddleware);
+
+io.on("connection", (socket) => {
+    const { username } = socket.request.session;
+    console.log(`[NEW CONNECTION] ${username} connected`);
+
+    socket.on("disconnect", () => {
+        console.log(`[DISCONNECTED] ${username} disconnected`);
+    });
+});
 
 httpServer.listen(8000, () => {
     console.log("listening on port 8000...");
