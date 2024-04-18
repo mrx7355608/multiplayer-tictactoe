602bae69d866dddec700a814918248d6bfab7831
mrx7355608 <mrx7355608@gmail.com>
2024-04-19 04:35:49 +0500

    added rematch functionality

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ io.on("connection", (socket) => {
     socket.on("play-game", () => {
         console.log(`[PLAY GAME] ${username} wants to play game`);
         socket.join(room.id);
+        socket.join(username);
         if (room.user1) {
             room.user2 = username;
         } else {
@@ -53,6 +54,23 @@ io.on("connection", (socket) => {
         }
     });
 
+    socket.on("ask-for-a-rematch", () => {
+        if (room.user1 === username) {
+            io.to(room.user2).emit("want-a-rematch", { username });
+        } else {
+            io.to(room.user1).emit("want-a-rematch", { username });
+        }
+    });
+
+    socket.on("rematch-accepted", () => {
+        room.board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""],
+        ];
+        io.to(room.id).emit("rematch-start", { board: room.board });
+    });
+
     socket.on("disconnect", () => {
         console.log(`[DISCONNECTED] ${username} disconnected`);
         if (username === room.user1) {
@@ -65,16 +83,74 @@ io.on("connection", (socket) => {
 });
 
 function checkWinner(board) {
+    // First diagonal check
     if (
         board[0][0] &&
         board[0][0] === board[1][1] &&
         board[0][0] === board[2][2]
     ) {
         return board[0][0];
-    } else if (
+    }
+
+    // Second diagonal check
+    else if (
         board[0][2] &&
         board[0][2] === board[1][1] &&
         board[0][2] === board[2][0]
+    ) {
+        return board[0][2];
+    }
+
+    // First horizontal line check
+    else if (
+        board[0][0] &&
+        board[0][0] === board[0][1] &&
+        board[0][0] === board[0][2]
+    ) {
+        return board[0][0];
+    }
+
+    // Second horizontal line check
+    else if (
+        board[1][0] &&
+        board[1][0] === board[1][1] &&
+        board[1][0] === board[1][2]
+    ) {
+        return board[1][0];
+    }
+
+    // Third horizontal line check
+    else if (
+        board[2][0] &&
+        board[2][0] === board[2][1] &&
+        board[2][0] === board[2][2]
+    ) {
+        return board[2][0];
+    }
+
+    // First vertical line check
+    else if (
+        board[0][0] &&
+        board[0][0] === board[1][0] &&
+        board[0][0] === board[2][0]
+    ) {
+        return board[0][0];
+    }
+
+    // Second vertical line check
+    else if (
+        board[0][1] &&
+        board[0][1] === board[1][1] &&
+        board[0][1] === board[2][1]
+    ) {
+        return board[0][1];
+    }
+
+    // Third vertical line check
+    else if (
+        board[0][2] &&
+        board[0][2] === board[1][2] &&
+        board[0][2] === board[2][2]
     ) {
         return board[0][2];
     }
